40bd88ae239bcd696e387ea397cb77c6208362ea
SpiveeWorks <spiveehere@gmail.com>
2018-04-12 09:07:33 +1000

    remove list based solution

--- lambda.hs
+++ lambda.hs
@@ -1,23 +1,7 @@
 import Data.Maybe
 import Data.List
 
-type OpenTerm a = [(String, Term a)] -> Term a
 data Term a = Fun (Term a -> Term a) | Value a
-
-close :: OpenTerm a -> Term a
-close x = x []
-
-lambda :: String -> OpenTerm a -> OpenTerm a
-lambda ident body ctx = Fun (\val -> body ((ident, val) : ctx))
-
-var :: String -> OpenTerm a
-var ident ctx = fromJust (lookup ident ctx)
-
-apply :: OpenTerm a -> OpenTerm a -> OpenTerm a
-apply f x ctx = applyTerm (f ctx) (x ctx)
-
-applyTerm :: Term a -> Term a -> Term a
-applyTerm (Fun f) = f
 
 extractTerm :: Term a -> a
 extractTerm (Value x) = x
@@ -25,39 +9,48 @@ extractTerm (Value x) = x
 extractFun :: Term a -> a -> a
 extractFun (Fun f) = extractTerm . f . Value
 
+buildFun :: (a -> a) -> Term a
+buildFun f = Fun (Value . f . extractTerm)
 
 
-idTerm :: Term a
-idTerm = close $ lambda "x" $ var "x"
-
-verboseId :: a -> a
-verboseId = extractFun idTerm
-
-
-data LambdaAST =
+data LambdaAST a =
   Variable String
-  | Lambda String LambdaAST
-  | Apply LambdaAST LambdaAST deriving Show
+  | Lambda String (LambdaAST a)
+  | Apply (LambdaAST a) (LambdaAST a)
+  | Term (Term a)
 
-transformAST :: LambdaAST -> Term a
-transformAST = close . transform
 
-transform :: LambdaAST -> OpenTerm a
-transform (Variable ident) = var ident
-transform (Lambda ident body) = lambda ident (transform body)
-transform (Apply f x) = apply (transform f) (transform x)
+transform :: LambdaAST a -> Term a
+transform (Lambda ident body) = Fun (\val -> transform (sub ident val body))
+transform (Apply f x) = applyTerm (transform f) (transform x)
+transform (Term result) = result
+
+sub :: String -> Term a -> LambdaAST a -> LambdaAST a
+sub ident val = go
+  where go (Lambda ident' body)
+          | ident' == ident = Lambda ident' body  -- shadowing
+          | otherwise = Lambda ident' (go body)
+        go (Apply f x) = Apply (go f) (go x)
+        go (Variable ident')
+          | ident' == ident = Term val
+          | otherwise = Variable ident'
+        go (Term other) = Term other
+
+
+applyTerm :: Term a -> Term a -> Term a
+applyTerm (Fun f) = f
 
 data ParseTree =
   Str String
   | Brackets [ParseTree]
 
-parseAST :: String -> LambdaAST
+parseAST :: String -> LambdaAST a
 parseAST ('\\':xs) = Lambda ident (parseAST body)
   where (ident, _ : body) = splitAt (fromJust (elemIndex '.' xs)) xs
 parseAST xs = foldl1 Apply $ map Variable $ words xs
 
 parseLambda :: String -> Term a
-parseLambda = transformAST . parseAST
+parseLambda = transform . parseAST
 
 parseFun :: String -> a -> a
 parseFun = extractFun . parseLambda
