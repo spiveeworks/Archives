1fdc870fe319800453b0e03d4ba91d10b94829a3
SpiveeWorks <spiveehere@gmail.com>
2023-01-02 23:39:21 +1100

    generate non-caterpillar Countdown solutions

--- Countdown.hs
+++ Countdown.hs
@@ -1,3 +1,5 @@
+import Control.Arrow (first, second)
+
 data Expr = L Int | Expr `Plus` Expr | Expr `Minus` Expr
             | Expr `Times` Expr | Expr `Divide` Expr
 
@@ -9,31 +11,39 @@ instance Show Expr where
   showsPrec i (x `Divide` y) = (++) "(" . showsPrec i x . (++) " / " . showsPrec i y . (++) ")"
 
 
+partitionsRaw :: [Int] -> [([Int], [Int])]
+partitionsRaw [] = [([], [])]
+partitionsRaw (x : xs) = map (first ((:) x)) cs ++ map (second ((:) x)) cs where
+  cs = partitionsRaw xs
+
+-- partitions without redundancy or empty parts
+partitions :: [Int] -> [([Int], [Int])]
+partitions [] = []
+partitions (x : xs) = [(x : ls, rs) | (ls, rs) <- partitionsRaw xs, not (null rs)]
+
+-- combinations = map fst . partitionsRaw
 combinations :: [Int] -> [[Int]]
 combinations [] = [[]]
 combinations (x : xs) = cs ++ map ((:) x) cs where cs = combinations xs
 
-chooseOne :: [Int] -> [(Int, [Int])]
-chooseOne [] = []
-chooseOne (x : xs) = (x, xs) : map (\(y, ys) -> (y, x:ys)) (chooseOne xs)
-
-appendExpr :: Int -> (Expr, Int) -> [(Expr, Int)]
-appendExpr x (yExpr, y) = divA ++ divB ++ subtract ++ add ++ mul where
-  divA = if x `mod` y == 0 then [(L x `Divide` yExpr, x `div` y)] else []
-  divB = if y `mod` x == 0 then [(yExpr `Divide` L x, y `div` x)] else []
-  subtract = if x > y then [(L x `Minus` yExpr, x - y)]
-                      else if x < y then [(yExpr `Minus` L x, y - x)]
+appendExpr :: (Expr, Int) -> (Expr, Int) -> [(Expr, Int)]
+appendExpr (xExpr, x) (yExpr, y) = divA ++ divB ++ subtract ++ add ++ mul where
+  divA = if x `mod` y == 0 then [(xExpr `Divide` yExpr, x `div` y)] else []
+  divB = if y `mod` x == 0 then [(yExpr `Divide` xExpr, y `div` x)] else []
+  subtract = if x > y then [(xExpr `Minus` yExpr, x - y)]
+                      else if x < y then [(yExpr `Minus` xExpr, y - x)]
                                     else []
-  add = [(L x `Plus` yExpr, x + y), (yExpr `Plus` L x, y + x)]
-  mul = [(L x `Times` yExpr, x * y), (yExpr `Times` L x, y * x)]
+  add = [(xExpr `Plus` yExpr, x + y), (yExpr `Plus` xExpr, y + x)]
+  mul = [(xExpr `Times` yExpr, x * y), (yExpr `Times` xExpr, y * x)]
 
 generateExprsStrict :: [Int] -> [(Expr, Int)]
+generateExprsStrict [] = []
 generateExprsStrict [x] = [(L x, x)]
-generateExprsStrict [x, y] = appendExpr x (L y, y)
 generateExprsStrict args = do
-  (arg, rest) <- chooseOne args
-  expr <- generateExprsStrict rest
-  appendExpr arg expr
+  (largs, rargs) <- partitions args
+  lexpr <- generateExprsStrict largs
+  rexpr <- generateExprsStrict rargs
+  appendExpr lexpr rexpr
 
 generateExprsLenient :: [Int] -> [(Expr, Int)]
 generateExprsLenient xs = combinations xs >>= generateExprsStrict
